a3a75925e2f17d00dfbe7e010b33abb2b03dcc2b
Igor Desh <desh_gambler@mail.ru>
2021-07-23 11:44:58 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ module.exports = function check(str, bracketsConfig) {
   if (str.length % 2 > 0) {
     return false;
   }
+  let length = str.length;
   let count = 0;
-  for(let j = 0; j < str.length / 2; j++) {
+  for(let j = 0; j < length / 2; j++) {
     for (let i = 0; i < bracketsConfig.length; i++) {
       str = str.replace(bracketsConfig[i].join(''), '');
     }
