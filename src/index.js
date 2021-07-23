69ee110bd51fea005d3902e0258afc9a9c7def08
Igor Desh <desh_gambler@mail.ru>
2021-07-23 11:42:53 +0300

    start project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if (str.length % 2 > 0) {
+    return false;
+  }
+  let count = 0;
+  for(let i = 0; i < str.length / 2; i++) {
+    for (let j = 0; j < bracketsConfig.length; j++) {
+      str = str.replace(bracketsConfig[j].join(''), '');
+    }
+    count++;
+  }
+  if(str.length > 0) return false
+  else return true
 }
